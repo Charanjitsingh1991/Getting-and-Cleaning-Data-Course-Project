280c32c325c50cd0cfddf7d1b1c97b5d2efb810e
Charanjit Singh <57300104+Dev-tutsin@users.noreply.github.com>
2020-06-05 01:04:27 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 #******************************************************************
-#Step 0. Downloading and unzipping dataset
+# Downloading and unzipping dataset
 #******************************************************************
 
 if(!file.exists("./data")){dir.create("./data")}
